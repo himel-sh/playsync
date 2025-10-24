49c66fd3a3af76a1cf0af4a505a1347e9b3bdf44
Himel <shahriarhasan221@gmail.com>
2025-10-24 13:47:18 +0600

    hid credentials

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCqCsSlgqKNXMeorC71vAE131H8OW-ReVA",
-  authDomain: "playsync-gamehub.firebaseapp.com",
-  projectId: "playsync-gamehub",
-  storageBucket: "playsync-gamehub.firebasestorage.app",
-  messagingSenderId: "921106081337",
-  appId: "1:921106081337:web:5620691a569d7fd73a4fe8",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
